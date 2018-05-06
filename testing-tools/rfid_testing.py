d6d3d03535fc087fa7a8db487385cc0ee03b372c
Luca <ticinoluca@yahoo.it>
2018-05-06 16:02:43 +0200

    Test tools

---
+++ testing-tools/rfid_testing.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+import SimpleMFRC522
+import config
+
+
+reader = SimpleMFRC522.SimpleMFRC522()
+try:
+    while True:
+        rfid_id = reader.read_id_no_block()
+        if rfid_id is not None:
+            print(rfid_id)
+            #time.sleep(0.2)
+finally:
+    GPIO.cleanup()
