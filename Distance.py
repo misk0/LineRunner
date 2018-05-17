0a0e5311fd05263a9a260e22067fe0256f25b502
luca.riva <ticinoluca@yahoo.it>
2018-05-17 15:11:06 +0200

    Distances functions

--- Distance.py
+++ Distance.py
@@ -32,5 +32,57 @@ def measure_distance(sensor_pos, debug=False):
 
     return complex_distance
 
+def follow_distance(debug=False):
+    distance_left = measure_distance(config.US_LEFT, False)
+    distance_middle = measure_distance(config.US_CENTER, False)
+    distance_right = measure_distance(config.US_RIGHT, False)
+
+    if debug:
+        print("Left distance: ", distance_left)
+        print("Middle distance: ", distance_middle)
+        print("Right distance: ", distance_right)
+
+    if distance_right <= config.Distance_MinValue:
+        config.dist_error = -4
+        if debug:
+            print("DistRight too small, go left")
+    elif distance_left <= config.Distance_MinValue:
+        config.dist_error = 4
+        if debug:
+            print("DistLeft too small, go right")
+    elif distance_right > distance_middle and distance_right > distance_left:
+        config.dist_error = 3
+        if debug:
+            print("DistRight big, go right")
+    elif distance_middle > distance_right and distance_middle > distance_left:
+        config.dist_error = 0
+        if debug:
+            print("Distmid big, go FW")
+    elif distance_left > distance_middle and distance_left > distance_right:
+        config.dist_error = -3
+        if debug:
+            print("DistLeft big, go left")
+
+    P = config.dist_error
+    config.dist_integrative = config.dist_integrative + config.dist_error
+    D = config.dist_error - config.dist_previous_error
+    PIDvalue = (config.dist_kp * P) + (config.dist_ki * config.dist_integrative) + (config.dist_kd * D)
+
+    if config.dist_left_speed - PIDvalue > 100:
+        config.walk_speed_left = 100
+    elif config.dist_left_speed - PIDvalue < config.min_left_speed:
+        config.walk_speed_left = 0
+    else:
+        config.walk_speed_left = config.dist_left_speed - PIDvalue
+
+    if config.dist_right_speed + PIDvalue > 100:
+        config.walk_speed_right = 100
+    elif config.dist_right_speed + PIDvalue < config.min_right_speed:
+        config.walk_speed_right = 0
+    else:
+        config.walk_speed_right = config.dist_right_speed + PIDvalue
+
+    config.dist_previous_error = config.dist_error
+
 
 
