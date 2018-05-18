1a4bb665d4afeabd5162bad6d111444c716075e4
luca.riva <ticinoluca@yahoo.it>
2018-05-18 16:55:30 +0200

    test

--- testing-tools/FollowMeBaby.py
+++ testing-tools/FollowMeBaby.py
@@ -2,63 +2,62 @@ import RPi.GPIO as GPIO
 import config
 from time import sleep
 
-class FollowMeBaby:
-    def __init__(self):
-        self._running = True
-        # GPIO.setmode(GPIO.BOARD)
+config.init()
 
-        self.pin = [38, 33, 36, 35, 37]
+pin = [config.line_follow_rmax, config.line_follow_lmax, config.line_follow_mid, config.line_follow_lmin, config.line_follow_rmin]
         # pin[0]: sensore esterno destra     //Nello schizzo S4
         # pin[4]: sensore interno destra     //Nello schizzo S2
         # pin[1]: sensore esterno sinistra   //Nello schizzo S5
         # pin[3]: sensore interno sinistra   //Nello schizzo S3
         # pin[2]: sensore centrale           //Nello schizzo S1
         # pin[5]: sensore centrale arretrato //Nello schizzo S6
-        i = 0
-        while i < 5:
-            GPIO.setup(self.pin[i], GPIO.IN)  # Assegnazione valore numerico ai pin sui quali verrà letto l'input
-            i = i + 1
-
-    def terminate(self):
-        self._running = False
 
 
-    def run(self):
-        try:
-            while True:
-                i = 0
-                while i < 5:
-                    i += 1
-                    if GPIO.input(self.pin[i]) == 1:
-                        print("PIN {} black".format(i))
+def FollowMe():
+    i = 0
+    while i < 5:
+        if GPIO.input(pin[i]) == 1:
+            print("PIN {} black".format(i))
+        i += 1
 
-                # # Va' capito se è possibile che sul pin[2] e sul pin[3] (o sul pin[2] e sul pin[4]) venga letto nero in contemporanea
-                # # v motore sinistro < v motore destro
-                # if GPIO.input(self.pin[3]) == 0 and GPIO.input(self.pin[2]) == 1 or GPIO.input(self.pin[3]) == 0 and GPIO.input(
-                #         self.pin[2]) == 0:
-                #     print("v motore sinistro < v motore destro")
-                #     while GPIO.input(pin[2]) == 1:
-                #         left_motor_speed = left_motor_speed - 0.05
+    # Curva dolce verso sinistra
+    if GPIO.input(config.line_follow_lmin) == 1 and GPIO.input(config.line_follow_mid) == 0 or GPIO.input(config.line_follow_lmin) == 1 and GPIO.input(config.line_follow_mid) == 1:
+        while GPIO.input(config.line_follow_mid) == 0:
+            config.walk_speed_left = config.walk_speed_left - 0.05
 
-                # # v motore sinistro > v motore destro
-                # if GPIO.input(self.pin[4]) == 0 and GPIO.input(self.pin[2]) == 1 or GPIO.input(self.pin[4]) == 0 and GPIO.input(
-                #         self.pin[2]) == 0:
-                #     print("# v motore sinistro > v motore destro")
-                #     while GPIO.input(pin[2]) == 1:
-                #         right_motor_speed = right_motor_speed - 0.05
-                #
-                # # fermare i motori
-                # # -1*(v motore destro) = v motore sinistro
-                # if GPIO.input(self.pin[2]) == 1 and GPIO.input(self.pin[3]) == 1 and GPIO.input(self.pin[4]) == 0:
-                #     print("-1*(v motore destro) = v motore sinistro")
-                #
-                # # fermare i motori
-                # # -1*(v motore sinistro) = v motore destro
-                # if GPIO.input(self.pin[2]) == 1 and GPIO.input(self.pin[3]) == 1:
-                #     print("-1*(v motore sinistro) = v motore destro")
-                #
-                # # v motore destro = v motore sinistro
-                # if GPIO.input(self.pin[2]) == 0:  # Se legge nero
-                #     print("v motore destro = v motore sinistro")
-        finally:
-            GPIO.cleanup()
+    # Curva dolce verso destra
+    if GPIO.input(config.line_follow_rmin) == 1 and GPIO.input(config.line_follow_mid) == 0 or GPIO.input(config.line_follow_rmin) == 1 and GPIO.input(config.line_follow_mid) == 1:
+    # print("# v motore sinistro > v motore destro")
+        while GPIO.input(config.line_follow_mid) == 0:
+             config.walk_speed_right = config.walk_speed_right - 0.05
+
+     # Curva a 90° verso destra
+     # -1*(v motore destro) = v motore sinistro
+    if GPIO.input(config.line_follow_mid) == 0 and GPIO.input(config.line_follow_lmin) == 0 and GPIO.input(config.line_follow_rmin) == 0 and GPIO.input(config.line_follow_rmax) == 0 and GPIO.input(config.line_follow_lmax) == 1:
+         while GPIO.input(config.line_follow_mid) == 0:
+            # GPIO.output(config.left_motor_direction_inv, GPIO.LOW)
+            # GPIO.output(config.left_motor_direction, GPIO.HIGH)
+            config.drive_right.ChangeDutyCycle(0)
+            GPIO.output(config.right_motor_direction, GPIO.LOW)
+            GPIO.output(config.right_motor_direction_inv, GPIO.HIGH)
+            config.drive_right.ChangeDutyCycle(config.walk_speed_right)
+
+                    # Curva a 90° verso sinistra
+                    # -1*(v motore sinistro) = v motore destro
+    if GPIO.input(config.line_follow_mid) == 0 and GPIO.input(config.line_follow_lmin) == 0 and GPIO.input(config.line_follow_rmin) == 0 and GPIO.input(config.line_follow_lmax) == 0 and GPIO.input(config.line_follow_rmax) == 1:
+        while GPIO.input(config.line_follow_mid) == 0:
+            config.drive_left.ChangeDutyCycle(0)
+            GPIO.output(config.left_motor_direction, GPIO.LOW)
+            GPIO.output(config.left_motor_direction_inv, GPIO.HIGH)
+            config.drive_right.ChangeDutyCycle(config.walk_speed_left)
+            # GPIO.output(config.right_motor_direction, GPIO.HIGH)
+            # GPIO.output(config.right_motor_direction_inv, GPIO.LOW)
+
+    # v motore destro = v motore sinistro
+    if GPIO.input(config.line_follow_mid) == 1:  # Se legge nero
+        #    print("v motore destro = v motore sinistro")
+        GPIO.output(config.left_motor_direction, GPIO.HIGH)
+        GPIO.output(config.left_motor_direction_inv, GPIO.LOW)
+        GPIO.output(config.right_motor_direction, GPIO.HIGH)
+        GPIO.output(config.right_motor_direction_inv, GPIO.LOW)
+
