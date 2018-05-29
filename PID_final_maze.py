de0740863946605466936a47033cfa16940d940a
luca.riva <ticinoluca@yahoo.it>
2018-05-29 14:56:20 +0200

    update for Wednesday tests

--- PID_final_maze.py
+++ PID_final_maze.py
@@ -10,59 +10,98 @@ def follow_distance(debug):
     print("right",right_dist)
     print("left",left_dist)
 
-    MazeLargeMin = 26
-    RobotLarge = 13.2
-    Mid = (MazeLargeMin - RobotLarge)/2 #6.4
-    troppoSin = 4
-    troppoDes = troppoSin
-    Step = (Mid - troppoSin)/4 #0.6
-    QuasiTroppoSin = Step + (Step/3) + troppoSin #4.8
-    QuasiTroppoDes = QuasiTroppoSin
-    UnPoPiuSin = (Step + (Step/3))*2 + troppoSin #5.6
-    UnPoPiuDes = UnPoPiuSin
+    TotalDist = right_dist+left_dist
+    # MazeLargeMin = 26
+    # RobotLarge = 13.2
+    # Mid = (MazeLargeMin - RobotLarge)/2 #6.4
+    # troppoSin = 4
+    # troppoDes = troppoSin
+    # Step = (Mid - troppoSin)/4 #0.6
+    # QuasiTroppoSin = Step + (Step/3) + troppoSin #4.8
+    # QuasiTroppoDes = QuasiTroppoSin
+    # UnPoPiuSin = (Step + (Step/3))*2 + troppoSin #5.6
+    # UnPoPiuDes = UnPoPiuSin
 
-    if right_dist > left_dist and right_dist > Mid:
-        config.dist_error = 5
-        print("estremo dx")
+    # if right_dist > left_dist and right_dist > Mid:
+    #     config.dist_error = 5
+    #     print("estremo dx")
+    #
+    # if left_dist > right_dist and left_dist > Mid:
+    #     config.dist_error = -5
+    #     print("estremo sx")
+    #
+    # if left_dist < troppoSin:
+    #     config.dist_error = 4
+    #     if debug:
+    #         print("troppo sin")
+    # if left_dist >= troppoSin and left_dist < QuasiTroppoSin:
+    #     config.dist_error = 3
+    #     if debug:
+    #         print("quasitroppo sin")
+    #
+    # if left_dist >= QuasiTroppoSin and left_dist < UnPoPiuSin:
+    #     config.dist_error = 2
+    #     if debug:
+    #         print("unpopiu sin")
+    # if left_dist >= UnPoPiuSin and left_dist < Mid:
+    #     config.dist_error = 1
+    #     if debug:
+    #         print("unpo sin")
+    # if left_dist >= Mid and right_dist >= Mid and abs(left_dist - right_dist) < 3:
+    #     config.dist_error = 0
+    #     if debug:
+    #         print("mid")
+    # if right_dist >= UnPoPiuDes and right_dist < Mid:
+    #     config.dist_error = -1
+    #     if debug:
+    #         print("unpo dx")
+    # if right_dist >= QuasiTroppoDes and right_dist < UnPoPiuDes:
+    #     config.dist_error = -2
+    #     if debug:
+    #         print("unpopiu dx")
+    # if right_dist >= troppoDes and right_dist < QuasiTroppoDes:
+    #     config.dist_error = -3
+    #     if debug:
+    #         print("quasitroppo dx")
+    # if right_dist < troppoDes:
+    #     config.dist_error = -4
+    #     if debug:
+    #         print("troppo dx")
 
-    if left_dist > right_dist and left_dist > Mid:
-        config.dist_error = -5
-        print("estremo sx")
-
-    if left_dist < troppoSin:
+    if left_dist <= (1/9)*TotalDist:
         config.dist_error = 4
         if debug:
             print("troppo sin")
-    if left_dist >= troppoSin and left_dist < QuasiTroppoSin:
+    if left_dist <= (2/9)*TotalDist and left_dist > (1/9)*TotalDist:
         config.dist_error = 3
         if debug:
             print("quasitroppo sin")
 
-    if left_dist >= QuasiTroppoSin and left_dist < UnPoPiuSin:
+    if left_dist <= (3/9)*TotalDist and left_dist > (2/9)*TotalDist:
         config.dist_error = 2
         if debug:
             print("unpopiu sin")
-    if left_dist >= UnPoPiuSin and left_dist < Mid:
+    if left_dist <= (4/9)*TotalDist and left_dist > (3/9)*TotalDist:
         config.dist_error = 1
         if debug:
             print("unpo sin")
-    if left_dist >= Mid and right_dist >= Mid and abs(left_dist - right_dist) < 3:
+    if left_dist > (4/9)*TotalDist and right_dist > (4/9)*TotalDist:
         config.dist_error = 0
         if debug:
             print("mid")
-    if right_dist >= UnPoPiuDes and right_dist < Mid:
+    if right_dist <= (4/9)*TotalDist and right_dist > (3/9)*TotalDist:
         config.dist_error = -1
         if debug:
             print("unpo dx")
-    if right_dist >= QuasiTroppoDes and right_dist < UnPoPiuDes:
+    if right_dist <= (3/9)*TotalDist and right_dist > (2/9)*TotalDist:
         config.dist_error = -2
         if debug:
             print("unpopiu dx")
-    if right_dist >= troppoDes and right_dist < QuasiTroppoDes:
+    if right_dist <= (2/9)*TotalDist and right_dist > (1/9)*TotalDist:
         config.dist_error = -3
         if debug:
             print("quasitroppo dx")
-    if right_dist < troppoDes:
+    if right_dist <= (1/9)*TotalDist:
         config.dist_error = -4
         if debug:
             print("troppo dx")
@@ -70,8 +109,8 @@ def follow_distance(debug):
 
 
     P = config.dist_error
-    D = config.dist_error - config.dist_previous_error
-    PIDvalue = (config.dist_kp * P) + (config.dist_kd * D)
+    #D = config.dist_error - config.dist_previous_error
+    PIDvalue = (config.dist_kp * P) #+ (config.dist_kd * D)
 
     print("PID",PIDvalue)
 
